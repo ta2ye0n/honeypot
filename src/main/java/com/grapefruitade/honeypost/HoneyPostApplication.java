a8fd71265deae5af6ad09f0545377032575abf2b
taeyeon <tyeon42051@naver.com>
2024-01-23 20:35:14 +0900

    setting

---
+++ src/main/java/com/grapefruitade/honeypost/HoneyPostApplication.java
@@ -0,0 +1,13 @@
+package com.grapefruitade.honeypost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyPostApplication.class, args);
+    }
+
+}
