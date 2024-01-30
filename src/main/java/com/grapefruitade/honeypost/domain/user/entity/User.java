bb283db04cdac0394ab93cbc3f355bf902d3af2c
yena5511 <ar01077422264@gmail.com>
2024-01-31 00:23:25 +0900

    Add : User 엔티티 클래스

---
+++ src/main/java/com/grapefruitade/honeypost/domain/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.grapefruitade.honeypost.domain.user.entity;
+
+import com.grapefruitade.honeypost.domain.user.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username; //회원 가입, 로그인 할 때 사용
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", unique = true, nullable = false)
+    private String nickname; // 글 작성할 때 사용
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
