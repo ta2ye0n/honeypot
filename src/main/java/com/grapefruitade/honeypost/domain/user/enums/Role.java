707b2b9d23aea00fdb8891715ac2fb59ec8361fa
yena5511 <ar01077422264@gmail.com>
2024-02-03 20:17:21 +0900

    Add : Role enum

---
+++ src/main/java/com/grapefruitade/honeypost/domain/user/enums/Role.java
@@ -0,0 +1,11 @@
+package com.grapefruitade.honeypost.domain.user.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+    ROLE_USER, ROLE_ADMIN;
+
+}
