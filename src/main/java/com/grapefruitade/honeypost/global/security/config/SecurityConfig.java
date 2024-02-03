88cdcf44af0e29b3ad6517e552fa46b39ed1eae2
yena5511 <ar01077422264@gmail.com>
2024-02-03 19:49:11 +0900

    Add : SecurityConfig 클래스

---
+++ src/main/java/com/grapefruitade/honeypost/global/security/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.grapefruitade.honeypost.global.security.config;
+
+import com.grapefruitade.honeypost.global.security.handler.JwtAccessDeniedHandle;
+import com.grapefruitade.honeypost.global.security.handler.JwtAuthenticationEntryPoint;
+import com.grapefruitade.honeypost.global.security.jwt.JwtFilter;
+import com.grapefruitade.honeypost.global.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final TokenProvider jwtTokenProvider;
+    private final JwtAccessDeniedHandle jwtAccessDeniedHandle;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptionConfig ->
+                        exceptionConfig.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandle)
+                )
+
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(HttpMethod.GET, "/user/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/join").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+
+                                .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+}
