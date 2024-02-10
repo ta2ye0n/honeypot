2d01b30fab00c7f2d48b9c17123df92d263bd132
yena5511 <ar01077422264@gmail.com>
2024-02-10 18:37:15 +0900

    Refactor : User로 주입

--- src/main/java/com/grapefruitade/honeypost/domain/user/enums/Role.java
+++ src/main/java/com/grapefruitade/honeypost/domain/user/enums/Role.java
@@ -2,10 +2,14 @@ package com.grapefruitade.honeypost.domain.user.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
 @RequiredArgsConstructor
 @Getter
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_USER, ROLE_ADMIN;
-
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
--- src/main/java/com/grapefruitade/honeypost/global/security/auth/PrincipalDetails.java
+++ src/main/java/com/grapefruitade/honeypost/global/security/auth/PrincipalDetails.java
@@ -1,4 +1,5 @@
 package com.grapefruitade.honeypost.global.security.auth;
+import com.grapefruitade.honeypost.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @ToString
 @AllArgsConstructor
@@ -14,18 +16,21 @@ import java.util.Collection;
 @Builder
 public class PrincipalDetails implements UserDetails {
 
-    private Long id;
-    private String username;
-    private String password;
-    private Collection<GrantedAuthority> role;
+    private User user;
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return role;
+        return Collections.singleton(user.getRole());
     }
     @Override
     public String getUsername() {
-        return this.username;
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
     }
 
     @Override
