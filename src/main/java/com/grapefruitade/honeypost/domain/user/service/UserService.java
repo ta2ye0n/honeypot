7b0946a85f1028cb04cb1ecb9f098983a87902de
yena5511 <ar01077422264@gmail.com>
2024-02-21 22:53:56 +0900

    Add : 유저페이지 로직

---
+++ src/main/java/com/grapefruitade/honeypost/domain/user/service/UserService.java
@@ -0,0 +1,47 @@
+package com.grapefruitade.honeypost.domain.user.service;
+
+import com.grapefruitade.honeypost.domain.comment.repository.CommentRepository;
+import com.grapefruitade.honeypost.domain.like.repository.LikeRepository;
+import com.grapefruitade.honeypost.domain.post.dto.InfoPost;
+import com.grapefruitade.honeypost.domain.post.entity.Post;
+import com.grapefruitade.honeypost.domain.post.repository.PostRepository;
+import com.grapefruitade.honeypost.domain.user.entity.User;
+import com.grapefruitade.honeypost.global.util.UserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserUtil userUtil;
+    private final PostRepository postRepository;
+    private final LikeRepository likeRepository;
+    private final CommentRepository commentRepository;
+
+    public List<InfoPost> getMyPost(){
+
+        User user = userUtil.currentUser();
+
+        List<Post> userPost = postRepository.findPostByAuthor(user);
+
+        return userPost.stream()
+                .map(post -> InfoPost.builder()
+                        .postId(post.getId())
+                        .author(post.getAuthor().getUsername())
+                        .title(post.getTitle())
+                        .content(post.getContent())
+                        .likes(likeRepository.countByPost(post))
+                        .comments(commentRepository.countByPost(post))
+                        .previewImage(post.getPreviewUrl())
+                        .build())
+                .collect(Collectors.toList());
+
+
+    }
+}
